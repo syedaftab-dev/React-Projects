76dbe8eb6cf26b9584f22d002e4b14994b9401f9
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2025-08-31 23:05:54 +0530

    Created Work section(without animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Work from './Components/Work'
 
 function App() {
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white font-["satoshi"]'>
+    <div className='w-full h-screen bg-zinc-900 text-white'>
       <Navbar />
+      <Work />
     </div>
   )
 }
